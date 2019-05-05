bcae2820c3db9a2af593e47a4e9dc2b8cb8f360b
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2019-05-05 12:54:29 +0300

    add files

---
+++ app/src/main/java/com/example/healthtracker/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.healthtracker;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    setTitle("Health Tracker");
+                    return true;
+                case R.id.navigation_bmi:
+                    setTitle("Body mass index");
+                    return true;
+                case R.id.navigation_eer:
+                    setTitle("Estimated Energy Requirement");
+                    return true;
+                case R.id.navigation_pht:
+                    setTitle("Personal Health Tracking");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
