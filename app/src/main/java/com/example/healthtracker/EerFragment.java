07726151c290839ba7f91e0b1fbca1c5ce2acf5d
Osman Marankoz <osman.marankoz@gmail.com>
2019-05-13 18:19:11 +0300

    Update EerFragment.java

--- app/src/main/java/com/example/healthtracker/EerFragment.java
+++ app/src/main/java/com/example/healthtracker/EerFragment.java
@@ -1,19 +1,77 @@
 package com.example.healthtracker;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import com.alespero.expandablecardview.ExpandableCardView;
+import com.example.healthtracker.core.Person;
+
+import java.lang.String;
+import com.alespero.expandablecardview.ExpandableCardView;
+import com.example.healthtracker.core.Person;
+
+import java.lang.String;
 
 
 public class EerFragment extends Fragment {
 
+    private Person newPeople;
+
+    public EerFragment() {
+        // Required empty public constructor
+        newPeople = new Person(45.1, 159);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final String[] values= {"Sedentary","Low Active", "Active", "Very Active"};
+
+        final NumberPicker PA = view.findViewById(R.id.eerPick);
+        final TextView eerText = view.findViewById(R.id.EerText);
+        PA.setMinValue(0);
+        PA.setMaxValue(values.length-1);
+        PA.setValue(newPeople.getHeight());
+        PA.setDisplayedValues(values);
+        eerText.setText("Current PA : " +newPeople.getEer());
+
+
+        PA.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                eerText.setText("Physical Activity Coefficients : " + values[newVal]);
+                newPeople.setEer(values[newVal]);
+            }
+        });
+    }
+
+
+
+
+    // TODO: Rename and change types and number of parameters
+    public static EerFragment newInstance(String param1, String param2) {
+        EerFragment fragment = new EerFragment();
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_eer, container, false);
     }
-
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
 }
